cd22e4742f2d40118c0fbd2d5574e958099805a2
MinimizeZen <MinimizeZen@outlook.com>
2024-01-09 18:52:18 -0500

    Calculator Refactor

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -1,17 +1,38 @@
 //Import readline-sync
 let readline = require('readline-sync');
 
+function prompt(msg) {
+  console.log(`=> ${msg}`)
+}
+function invalidNumber(num) {
+  return num.trimStart() === "" || Number.isNaN(Number(num))
+}
 //Welcome message
-console.log("Welcome to Calculator");
+prompt("Welcome to Calculator");
 // Ask the user for the first number.
-console.log("What's the first number?");
-let num1 = Number(readline.question())
+prompt("What's the first number?");
+let num1 = readline.question()
+while (invalidNumber(num1)) {
+  prompt("That's not a number")
+  num1 = readline.question()
+}
+num1 = Number(num1)
 // Ask the user for the second number.
-console.log("What's the second number?");
-let num2 = Number(readline.question())
+prompt("What's the second number?");
+let num2 = readline.question()
+
+while (invalidNumber(num2)) {
+  prompt("That's not a number")
+  num2 = readline.question()
+}
+num2 = Number(num2)
 // Ask the user for an operation to perform.
-console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
 let operation = readline.question()
+while (!["1", "2", "3", "4"].includes(operation)) {
+  prompt("That's not a valid operation enter 1, 2, 3, or 4.")
+  operation = readline.question()
+}
 // Perform the operation on the two numbers.
 let output;
 switch (operation) {
