c90ab7303936ea68b7acf742ffd2790d8f63940d
MinimizeZen <MinimizeZen@outlook.com>
2024-01-08 21:38:12 -0500

    made sqfeet a constant

--- SmallProblemsEasy1-HowBig.js
+++ SmallProblemsEasy1-HowBig.js
@@ -1,7 +1,8 @@
 let rlSync = require('readline-sync');
+const SQMETERS_TO_FEET = 10.7639
 console.log('Enter the length of the room in meters: ')
 let rLength = rlSync.prompt()
 console.log('Enter the width of the room in meters: ')
 let rWidth = rlSync.prompt()
-console.log(`The area of the room is ${rLength * rWidth} square meters (${(rLength * rWidth * 10.7639).toFixed(2)})`)
+console.log(`The area of the room is ${rLength * rWidth} square meters (${(rLength * rWidth * SQMETERS_TO_FEET).toFixed(2)})`)
 
