38db74d2547f598216830d3dd50e67538d40e845
MinimizeZen <MinimizeZen@outlook.com>
2024-01-11 14:55:54 -0500

    Eslint and refactored loan calculator

--- LoanCalculator/LoanCalculator.js
+++ LoanCalculator/LoanCalculator.js
@@ -14,24 +14,30 @@ while (true) {
     prompt("Please enter an actual positive number");
     loanAmount = readline.question();
   }
-  loanAmount = Number(loanAmount)
+  loanAmount = Number(loanAmount);
   prompt("What is your interest rate(as a percentage)?");
   let loanInterest = readline.question().replace("%", "");
   while (invalidNumber(loanInterest)) {
     prompt("Please enter an actual positive number");
     loanInterest = readline.question().replace("%", "");
   }
-  loanInterest = Number(loanInterest) / 100
+  loanInterest = Number(loanInterest) / 100;
   prompt("What is the loan duration(in months)?");
   let loanDuration = readline.question();
   while (invalidNumber(loanDuration)) {
     prompt("Please enter an actual positive number");
     loanDuration = readline.question();
   }
-  loanDuration = Number(loanDuration)
-  let monthlyLoanInterest = loanInterest / 12
-  let monthlyPayment = loanAmount * (monthlyLoanInterest / (1 - Math.pow(1 + monthlyLoanInterest, loanDuration * -1)))
-  prompt(`Your monthlyPayment is $${monthlyPayment.toFixed(2)}`)
-  prompt("Continue? y|n ")
-  if (readline.question().toLowerCase() !== 'y') { break; }
+  loanDuration = Number(loanDuration);
+  let monthlyLoanInterest = loanInterest / 12;
+  let monthlyPayment = loanAmount *
+    (monthlyLoanInterest /
+      (1 - Math.pow(1 +
+        monthlyLoanInterest, loanDuration * -1)));
+  prompt(`Your monthlyPayment is $${monthlyPayment.toFixed(2)}`);
+  prompt("Continue? y|n ");
+  if (readline.question()
+    .toLowerCase() !== 'y') {
+    break;
+  }
 }
